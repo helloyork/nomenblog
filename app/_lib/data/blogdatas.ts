d701dbbd9fae81a3db6f1f30068a23943470a817
Nomen <111544765+helloyork@users.noreply.github.com>
2024-05-15 22:34:06 -0700

    Update blogdatas.ts
    Fix 404

--- app/_lib/data/blogdatas.ts
+++ app/_lib/data/blogdatas.ts
@@ -1,7 +1,7 @@
 import { Blog } from "./blogs";
 
 export const Blogs: Record<string, Blog> = {
-    "AIAPA v0.1.13": {
+    "aiapa-v0-1-13": {
         title: "AIAPA v0.1.13",
         href: "aiapa-v0-1-13",
         date: "2024-5-15",
